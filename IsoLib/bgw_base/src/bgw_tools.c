f0a01d5e5186e85d12900f3b3c5922ee5efc285e
tejedapastor <sergio.tejeda.pastor@hhi.fraunhofer.de>
2026-06-23 15:12:48 +0200

    Correct copyright declaration in ITU-T T.261 new files

--- IsoLib/bgw_base/src/bgw_tools.c
+++ IsoLib/bgw_base/src/bgw_tools.c
@@ -1,28 +1,27 @@
 /***********************************************************************************
- 
  This software module was originally developed by
- 
+
  Fraunhofer HHI
- 
- in the course of development of the T.261 for reference purposes and its
+
+ in the course of development of the ISO/IEC 23003-8 / ITU-T T.261 for reference purposes and its
  performance may not have been optimized. This software module is an implementation
- of one or more tools as specified by the T.261 standard. ISO/IEC gives
+ of one or more tools as specified by the ISO/IEC 23003-8 / ITU-T T.261 standard. ISO/IEC gives
  you a royalty-free, worldwide, non-exclusive, copyright license to copy, distribute,
  and make derivative works of this software module or modifications  thereof for use
- in implementations or products claiming conformance to the ISO/IEC T.261 standard
+ in implementations or products claiming conformance to the  ISO/IEC 23003-8 / ITU-T T.261 standard
  and which satisfy any specified conformance criteria. Those intending to use this
  software module in products are advised that its use may infringe existing patents.
  ISO/IEC have no liability for use of this software module or modifications thereof.
- Copyright is not released for products that do not conform to the ISO/IEC 23003-4
+ Copyright is not released for products that do not conform to the ISO/IEC 23003-8 / ITU-T T.261
  standard.
  
  Fraunhofer HHI retains full right to modify and use the code for its
  own purpose, assign or donate the code to a third party and to inhibit third parties
  from using the code for products that do not conform to MPEG-related ITU Recommenda-
  tions and/or ISO/IEC International Standards.
- 
+
  This copyright notice must be included in all copies or derivative works.
- 
+
  Copyright (c) ISO/IEC 2026.
  
  ***********************************************************************************/
@@ -242,16 +241,6 @@ int parse_input_params(int argc, char* argv[], bgw_params *parameters) {
 				case 'n':
 					{
 						parameters->normative_encoder_flag = atoi(argv[paramIndex + 1]);
-						break;
-					}
-				case 's':
-					{
-						parameters->substream_present_flag = atoi(argv[paramIndex + 1]);
-						break;
-					}
-				case 'c':
-					{
-						parameters->cg_info_present_flag = atoi(argv[paramIndex + 1]);
 						break;
 					}
 				case 'd':
